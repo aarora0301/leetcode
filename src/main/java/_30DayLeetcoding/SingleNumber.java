5a9a6903368ef4441ffbb23aab35f5a500fa9c35
aarshi <aarora0301@gmail.com>
2020-04-04 22:57:49 +0530

    Single Number

---
+++ src/main/java/_30DayLeetcoding/SingleNumber.java
@@ -0,0 +1,33 @@
+package main.java._30DayLeetcoding;
+
+/***
+ * Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+ *
+ * Note:
+ *
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ *
+ * Input: [2,2,1]
+ * Output: 1
+ * Example 2:
+ *
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ */
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        SingleNumber test = new SingleNumber();
+        System.out.println(test.singleNumber(new int[]{4,1,2,2,1}));
+    }
+}
