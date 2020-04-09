07283de91206de0c35b1a39353ef70a44200f806
aarshi <aarora0301@gmail.com>
2020-04-10 00:51:54 +0530

    Middle of linked list

---
+++ src/main/java/_30DayLeetcoding/MiddleOfLinkedList.java
@@ -0,0 +1,58 @@
+package main.java._30DayLeetcoding;
+
+
+/***
+ * Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+ *
+ * If there are two middle nodes, return the second middle node.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: [1,2,3,4,5]
+ * Output: Node 3 from this list (Serialization: [3,4,5])
+ * The returned node has value 3.  (The judge's serialization of this node is [3,4,5]).
+ * Note that we returned a ListNode object ans, such that:
+ * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, and ans.next.next.next = NULL.
+ */
+public class MiddleOfLinkedList {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        if (fast.next != null) {
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+
+    public static void main(String[] args) {
+        ListNode test1 = new ListNode(1);
+        test1.next = new ListNode(2);
+        test1.next.next = new ListNode(3);
+        test1.next.next.next = new ListNode(4);
+        test1.next.next.next.next = new ListNode(5);
+        //     test1.next.next.next.next.next = new ListNode(6);
+        MiddleOfLinkedList test = new MiddleOfLinkedList();
+        System.out.println(test.middleNode(test1).val);
+
+    }
+
+
+}
+
