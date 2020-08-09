c34442693009805e88ee6d457c0c7ad729e2d020
aarora0301 <aarora0301@gmail.com>
2020-08-10 00:00:07 +0530

    Min STack

---
+++ src/main/java/AmazonFAQ/stackAndQueue/MinStack.java
@@ -0,0 +1,41 @@
+package main.java.AmazonFAQ.stackAndQueue;
+
+import java.util.Stack;
+
+public class MinStack {
+
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty() || minStack.peek() >= x) {
+            minStack.push(x);
+        }
+    }
+
+    public void pop() {
+        int x = stack.pop();
+        if (minStack.peek() == x) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
