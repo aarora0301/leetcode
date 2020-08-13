4f0eadd7d2aead4bba103a470ca8d73928eccea5
aarora0301 <aarora0301@gmail.com>
2020-08-14 01:44:24 +0530

    FriendCircles : Union Find Operation

---
+++ src/main/java/AmazonFAQ/UnionFind/FriendCircles.java
@@ -0,0 +1,28 @@
+package main.java.AmazonFAQ.UnionFind;
+
+
+/****
+ * There are N students in a class. Some of them are friends, while some are not. Their friendship is transitive in nature.
+ * For example, if A is a direct friend of B, and B is a direct friend of C, then A is an indirect friend of C. And we defined a
+ * friend circle is a group of students who are direct or indirect friends.
+ *
+ * Given a N*N matrix M representing the friend relationship between students in the class. If M[i][j] = 1, then the ith and
+ * jth students are direct friends with each other, otherwise not. And you have to output the total number of friend circles among
+ * all the students.
+ */
+public class FriendCircles {
+
+    public int findCircleNum(int[][] M) {
+
+        int N = M.length;
+        final UF uf = new UF(N);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (M[i][j] == 1) {
+                    uf.union(i, j);
+                }
+            }
+        }
+        return uf.count();
+    }
+}
