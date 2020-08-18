3a6d427178443a904a81a4d92d8aa2b77aad28bb
aarora0301 <aarora0301@gmail.com>
2020-08-19 01:55:48 +0530

    Network connected: Union Find

---
+++ src/main/java/AmazonFAQ/UnionFind/MostStonesRemoved.java
@@ -0,0 +1,4 @@
+package main.java.AmazonFAQ.UnionFind;
+
+public class MostStonesRemoved {
+}
---
+++ src/main/java/AmazonFAQ/UnionFind/NetworkConnected.java
@@ -0,0 +1,28 @@
+package main.java.AmazonFAQ.UnionFind;
+
+
+/***
+ * There are n computers numbered from 0 to n-1 connected by ethernet cables connections forming a network where
+ * connections[i] = [a, b] represents a connection between computers a and b. Any computer can reach any other computer directly
+ * or indirectly through the network.
+ *
+ * Given an initial computer network connections. You can extract certain cables between two directly connected computers, and
+ * place them between any pair of disconnected computers to make them directly connected. Return the minimum number of times you
+ * need to do this in order to make all the computers connected. If it's not possible, return -1.
+ *
+ *
+ */
+public class NetworkConnected {
+
+    public int makeConnected(int n, int[][] connections) {
+
+        if (connections.length < n - 1) {
+            return -1;
+        }
+        final UF uf = new UF(n);
+        for (int[] singleConnection : connections) {
+            uf.union(singleConnection[0], singleConnection[1]);
+        }
+        return uf.count()-1;
+    }
+}
