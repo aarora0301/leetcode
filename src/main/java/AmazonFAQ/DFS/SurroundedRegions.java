c386bf3f9bb57c0e2876b7b6a4819956d5cc9df6
aarora0301 <aarora0301@gmail.com>
2020-08-27 01:24:58 +0530

    Add comments

--- src/main/java/AmazonFAQ/DFS/SurroundedRegions.java
+++ src/main/java/AmazonFAQ/DFS/SurroundedRegions.java
@@ -25,47 +25,65 @@ package main.java.AmazonFAQ.DFS;
  * if they are adjacent cells connected horizontally or vertically.
  */
 public class SurroundedRegions {
-    public void solve(char[][] board) {
-        if (board == null || board.length == 0) {
-            return;
-        }
 
+    public void solve(char[][] board) {
+        if(board.length == 0) return;
         int row = board.length;
         int col = board[0].length;
-        for (int i = 0; i < col; i++) {
-            if (board[0][i] == 'O')
+        /**
+         * Traverse first row and last column
+         * so boundary cells and its neighbour should not be converted to 'X'
+         * so mark them with '*', indicates that cell is visited
+         */
+        for(int i = 0; i < col; i++){
+            if(board[0][i] == 'O')
                 dfs(board, 0, i);
-
-            if (board[row - 1][i] == 'O')
+            if(board[row- 1][i] == 'O')
                 dfs(board, row - 1, i);
         }
-
-        for (int i = 0; i < row; i++) {
-            if (board[i][0] == 'O')
+        /***
+         * Traverse first row and last row
+         *  so boundary cells and its neighbour should not be converted to 'X'
+         *  so mark them with '*', indicates that cell is visited
+         */
+        for(int i = 0; i < row; i++){
+            if(board[i][0] == 'O')
                 dfs(board, i, 0);
-
-            if (board[i][col - 1] == 'O')
+            if(board[i][col - 1] == 'O')
                 dfs(board, i, col - 1);
         }
 
+        /***
+         *  Traverse the entire board again
+         *  this time
+         *  perform two steps:
+         *   1. if cell is marked '*' means it is not surrounded so convert it back to 'O'
+         *   2. if cell is marked 'O' means it can be surrounded so convert it into 'X'
+         */
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
-                if (board[i][j] == '*') {
-                    board[i][j] = 'O';
-                } else if (board[i][j] == 'O')
+                if (board[i][j] == 'O') {
                     board[i][j] = 'X';
+                }else if (board[i][j] == '*') {
+                    board[i][j] = 'O';
+                }
             }
         }
+
     }
 
-    public void dfs(char[][] board, int row, int col) {
-        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || board[row][col] != 'O') {
+    private void dfs(char[][] board, int row, int col) {
+        /**
+         *  board[row][col]!='O' means cell is already visited as dfs block can be entered only when
+         *   board[row][col]=='O'
+         */
+        if (row < 0 || col < 0 || row >= board.length - 1 || col >= board[0].length - 1 || board[row][col] != 'O') {
             return;
         }
         board[row][col] = '*';
         dfs(board, row + 1, col);
-        dfs(board, row, col + 1);
         dfs(board, row - 1, col);
+        dfs(board, row, col + 1);
         dfs(board, row, col - 1);
     }
 }
