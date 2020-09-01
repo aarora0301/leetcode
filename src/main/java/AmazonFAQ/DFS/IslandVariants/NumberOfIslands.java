5d3651bb418e373a3f1e55d39d8e13326bec1c54
aarora0301 <aarora0301@gmail.com>
2020-09-02 00:47:47 +0530

    Number of Islands: DFS

---
+++ src/main/java/AmazonFAQ/DFS/IslandVariants/NumberOfIslands.java
@@ -0,0 +1,53 @@
+package main.java.AmazonFAQ.DFS.IslandVariants;
+
+/***
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is
+ * formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+ *
+ * Input: grid = [
+ *   ["1","1","1","1","0"],
+ *   ["1","1","0","1","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","0","0","0"]
+ * ]
+ * Output: 1
+ *
+ * Input: grid = [
+ *   ["1","1","0","0","0"],
+ *   ["1","1","0","0","0"],
+ *   ["0","0","1","0","0"],
+ *   ["0","0","0","1","1"]
+ * ]
+ */
+public class NumberOfIslands {
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int ans = 0;
+        int row = grid.length, col = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') {
+            return;
+        }
+
+        grid[row][col] = '0';
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+    }
+
+}
