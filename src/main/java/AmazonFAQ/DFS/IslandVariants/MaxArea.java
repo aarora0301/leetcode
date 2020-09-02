3cd5cc208b9d9f428ffb4ff77d5068b1bf2d8b77
aarora0301 <aarora0301@gmail.com>
2020-09-02 23:06:40 +0530

    MAx Area : DFS

---
+++ src/main/java/AmazonFAQ/DFS/IslandVariants/MaxArea.java
@@ -0,0 +1,63 @@
+package main.java.AmazonFAQ.DFS.IslandVariants;
+
+
+/*****
+ * Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+ *
+ * Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+ *
+ * Example 1:
+ *
+ * [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,1,1,0,1,0,0,0,0,0,0,0,0],
+ *  [0,1,0,0,1,1,0,0,1,0,1,0,0],
+ *  [0,1,0,0,1,1,0,0,1,1,1,0,0],
+ *  [0,0,0,0,0,0,0,0,0,0,1,0,0],
+ *  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+ *  [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+ * Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+ */
+public class MaxArea {
+
+    int count = 0;
+
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int maxArea = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == 1) {
+                    count = 0;
+                    dfs(grid, i, j);
+                    maxArea = Math.max(maxArea, count);
+                }
+            }
+        }
+        return maxArea;
+    }
+
+    private void dfs(int[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == 0 || grid[row][col] == -1) {
+            return;
+        }
+        grid[row][col] = -1;
+        count++;
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+
+    }
+
+
+    public static void main(String[] args) {
+        MaxArea mx = new MaxArea();
+        int[][] grid = new int[][]{{1}};
+        System.out.println(mx.maxAreaOfIsland(grid));
+    }
+}
