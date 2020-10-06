a527631af1c0d22bc4ad7771f9cc3a39aa3b5733
aarora0301 <aarora0301@gmail.com>
2020-10-07 01:14:32 +0530

    Number Of Islands : DFS Revision

---
+++ src/main/java/Patterns/NumberOfIslands.java
@@ -0,0 +1,37 @@
+package main.java.Patterns;
+
+public class NumberOfIslands {
+
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int count = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private void dfs(char[][] grid, int row, int col) {
+        if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length || grid[row][col] == '0') {
+            return;
+        }
+        grid[row][col] = '0';
+        dfs(grid, row - 1, col);
+        dfs(grid, row + 1, col);
+        dfs(grid, row, col - 1);
+        dfs(grid, row, col + 1);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
